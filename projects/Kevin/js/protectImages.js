17324c83a7ece66ed4f2be25bf6ce4a2bf5bd903
Sky Roth <55454027+concordgrape@users.noreply.github.com>
2021-08-05 16:35:51 -0400

    Add files via upload

---
+++ projects/Kevin/js/protectImages.js
@@ -0,0 +1,20 @@
+/*
+    FILE            :   protectImages.js
+    CREATED ON      :   March 11, 2021
+    LAST UPDATED    :   March 11, 2021
+    CREATED BY      :   Sky Roth
+
+    PURPOSE         :   This file will protect the background image from being saved from the user, or dragging it around
+*/
+
+$(document).ready(function() {
+    $('#backgroundImg').on('dragstart', function(e) {
+        e.preventDefault()
+    });
+});
+
+//  Obfuscator -> Last updated: March 11, 2021
+
+/*
+var _0x21cc=['#backgroundImg','444187FaxVxD','1SqWFQX','199788jVzKcE','261005QmFkuQ','2JwQEUA','50cQqaNf','21805iKlyrE','1465038eaBVeC','1246253YjXEOf','preventDefault','dragstart','1CcgHls','ready','407881TnhBQe','6xbQehM'];var _0x1010=function(_0x3df44a,_0x185081){_0x3df44a=_0x3df44a-0xc3;var _0x21ccc3=_0x21cc[_0x3df44a];return _0x21ccc3;};var _0x43e058=_0x1010;(function(_0x56a2be,_0x3ae1d0){var _0x5d690c=_0x1010;while(!![]){try{var _0x40b7d0=parseInt(_0x5d690c(0xc4))*parseInt(_0x5d690c(0xc9))+parseInt(_0x5d690c(0xc7))*parseInt(_0x5d690c(0xce))+parseInt(_0x5d690c(0xc6))*-parseInt(_0x5d690c(0xd1))+parseInt(_0x5d690c(0xc8))+parseInt(_0x5d690c(0xcb))*-parseInt(_0x5d690c(0xcc))+-parseInt(_0x5d690c(0xcd))+-parseInt(_0x5d690c(0xca))*-parseInt(_0x5d690c(0xc3));if(_0x40b7d0===_0x3ae1d0)break;else _0x56a2be['push'](_0x56a2be['shift']());}catch(_0x4a7302){_0x56a2be['push'](_0x56a2be['shift']());}}}(_0x21cc,0xca3c6),$(document)[_0x43e058(0xd2)](function(){var _0x488585=_0x43e058;$(_0x488585(0xc5))['on'](_0x488585(0xd0),function(_0x32505e){var _0x523690=_0x488585;_0x32505e[_0x523690(0xcf)]();});}));
+*/
