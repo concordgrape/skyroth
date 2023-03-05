09ace96eeff7b5bf8677c44af450e60a6de83717
Sky Roth <sky.j.roth@gmail.com>
2023-03-05 13:50:03 -0500

    init

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Links</h1>
+      <h2><a href="http://www.yea.dev" target="_blank">yea.dev</a> (still under development)</h2>
+      <h2><a href="http://www.github.com/concordgrape" target="_blank">github.com/concordgrape</a></h2>
+      <hr />
+      <h1>Changelog</h1>
+      <h2><a href="http://www.loremgen.com" target="_blank">LoremGen.com</a></h2>
+      <h3>03/05/23</h3>
+      <ul>
+        <li>Initial Release</li>
+        <li>Decreased default number of generate paragraphs</li>
+        <li>Created original passage section</li>
+      </ul>  
+      <h2><a href="http://www.loremgen.com" target="_blank">white_border - App</a></h2>
+      <h3>03/02/23</h3>
+      <ul>
+        <li>Bug Fixes</li>
+        <li>Fixed Settings page glitch (Settings page wasn't appearing on specific devices)</li>
+      </ul>  
+    </div>
+  );
+}
+
+export default App;
