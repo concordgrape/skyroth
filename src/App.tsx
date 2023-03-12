1edb52068f6ef3ebfb498ad35e29ed424de76095
Sky Roth <sky.j.roth@gmail.com>
2023-03-11 22:27:32 -0500

    update

--- src/App.tsx
+++ src/App.tsx
@@ -4,8 +4,9 @@ function App() {
   return (
     <div>
       <h1>Links</h1>
-      <h2><a href="http://www.yea.dev" target="_blank">yea.dev</a> (still under development)</h2>
+      <h2><a href="http://www.yea.dev" target="_blank">yea.dev</a></h2>
       <h2><a href="http://www.github.com/concordgrape" target="_blank">github.com/concordgrape</a></h2>
+      <h3>still adding stuff here lol</h3>
       <hr />
       <h1>Changelog</h1>
       <h2><a href="http://www.loremgen.com" target="_blank">LoremGen.com</a></h2>
