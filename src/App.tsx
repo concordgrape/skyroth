8aac5dea0412c928bdff43738d7ed7541411bfba
Sky Roth <sky.j.roth@gmail.com>
2023-05-22 12:08:05 -0400

    privacy policy

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 
 import Home from './Home';
+import BorderApp from './BorderApp';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+      <Route path="/white_border" element={<BorderApp />} />
       <Route path="*" element={<Home />} />
     </Routes>
   );
---
+++ src/BorderApp.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+
+function BorderApp() {
+  return (
+    <div>
+        <h1>white_border - Privacy Policy</h1>
+        <h2>Last Updated: May 22, 2023</h2>
+        <hr></hr>
+        
+        Welcome to our privacy policy.<br></br> Your privacy is critically important to us. At white_border, we have a few fundamental principles:<br></br>
+
+        <br></br>
+        We don’t ask you for personal information unless we truly need it.
+        We don’t share your personal information with anyone except to comply with the law, develop our products, or protect our rights.
+        We don’t store personal information unless required for the on-going operation of our services.
+        This Privacy Policy describes the policies and procedures of white_border on the collection, use and disclosure of your information when you use the white_border Application, and tells you about your privacy rights and how the law protects you.
+
+        By using the Application, you agree to the collection and use of information in accordance with this Privacy Policy.
+        <hr></hr>
+        <br></br>
+        <h2>1. INFORMATION WE COLLECT</h2>
+
+        We don't collect or store any personal data from you while you use our application. However, we do utilize Google Analytics to understand how our application is used and to improve it. Google Analytics collects statistical data about our users' behavior in our Application, but it does not identify individual users to us.
+
+        For a better understanding of what data Google Analytics collects and how it is used, we recommend that you review Google's privacy policy at https://policies.google.com/privacy.
+
+        <h2>2. HOW WE USE THE INFORMATION WE COLLECT</h2>
+
+        The only purpose for which we use the data collected via Google Analytics is to improve our Application, and to understand what our users prefer. This statistical data does not allow us to identify you personally.
+
+        <h2>3. CHILDREN'S PRIVACY</h2>
+
+        Our Application does not address anyone under the age of 13. We do not knowingly collect personally identifiable information from children under 13. In the case we discover that a child under 13 has provided us with personal information, we immediately delete this from our servers. If you are a parent or guardian and you are aware that your child has provided us with personal information, please contact us so that we will be able to take necessary actions.
+
+        <h2>4. CHANGES TO THIS PRIVACY POLICY</h2>
+
+        We may update our Privacy Policy from time to time. Thus, we advise you to review this page periodically for any changes. We will notify you of any changes by posting the new Privacy Policy on this page. These changes are effective immediately after they are posted on this page.
+
+        <h2>5. CONTACT US</h2>
+
+        If you have any questions or suggestions about our Privacy Policy, do not hesitate to contact us.
+        
+        <br></br><br></br>
+        <a href = "mailto: sky@yea.dev">sky@yea.dev</a>
+    </div>
+  );
+}
+
+export default BorderApp;
