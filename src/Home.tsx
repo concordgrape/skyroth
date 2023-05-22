22092deea1876d49425e198a627844c2d39cb02e
Sky Roth <sky.j.roth@gmail.com>
2023-05-22 11:41:50 -0400

    updated home

--- src/Home.tsx
+++ src/Home.tsx
@@ -9,18 +9,21 @@ function Home() {
         <h3>still adding stuff here lol</h3>
         <hr />
         <h1>Changelog</h1>
+        <h2><a href="https://apps.apple.com/ca/app/white-border/id1574787497" target="_blank">white_border - App</a></h2>
+        <h3>05/22/23</h3>
+        <h4>v1.2</h4>
+        <ul>
+            <li>Bug Fixes</li>
+            <li>Add new guide lines</li>
+            <li>Removed some animations to improve performance</li>
+        </ul>  
         <h2><a href="http://www.loremgen.com" target="_blank">LoremGen.com</a></h2>
         <h3>03/05/23</h3>
+        <h4>v1.0</h4>
         <ul>
             <li>Initial Release</li>
             <li>Decreased default number of generated paragraphs</li>
             <li>Created original passage section</li>
-        </ul>  
-        <h2><a href="https://apps.apple.com/ca/app/white-border/id1574787497" target="_blank">white_border - App</a></h2>
-        <h3>03/02/23</h3>
-        <ul>
-            <li>Bug Fixes</li>
-            <li>Fixed Settings page glitch (Settings page wasn't appearing on specific devices)</li>
         </ul>  
     </div>
   );
