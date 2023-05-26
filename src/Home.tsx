efc6984cce3daa3cb1e5354637b0748aca63461e
Sky Roth <sky.j.roth@gmail.com>
2023-05-25 22:44:27 -0400

    update

--- src/Home.tsx
+++ src/Home.tsx
@@ -6,6 +6,7 @@ function Home() {
         <h1>Links</h1>
         <h2><a href="http://www.yea.dev" target="_blank">yea.dev</a></h2>
         <h2><a href="http://www.github.com/concordgrape" target="_blank">github.com/concordgrape</a></h2>
+        <h2><a href="mailto:sky@yea.dev" target="_blank">sky@yea.dev</a></h2>
         <h3>still adding stuff here lol</h3>
         <hr />
         <h1>Changelog</h1>
