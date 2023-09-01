ee74316e1dffe3dbc1ec760872bcb7205ebfb549
Sky Roth <sky.j.roth@gmail.com>
2023-09-01 06:48:07 -0400

    remove email

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 
+//        <h2><a href="mailto:sky@yea.dev" target="_blank">sky@yea.dev</a></h2>
+
 function Home() {
   return (
     <div>
         <h1>Links</h1>
         <h2><a href="http://www.yea.dev" target="_blank">yea.dev</a></h2>
         <h2><a href="http://www.github.com/concordgrape" target="_blank">github.com/concordgrape</a></h2>
-        <h2><a href="mailto:sky@yea.dev" target="_blank">sky@yea.dev</a></h2>
         <h3>still adding stuff here lol</h3>
         <hr />
         <h1>Changelog</h1>
