70e2c71390c2b0a6a6943d783537f2d8f6b039d0
Sky Roth <sky.j.roth@gmail.com>
2024-10-25 17:06:31 -0400

    update privacy policy (verbuu)

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,16 @@ import { Route, Routes } from 'react-router-dom';
 
 import Home from './Pages/Home';
 import BorderApp from './Pages/BorderApp';
+import DermiApp from './Pages/DermiApp';
+import VerbuuApp from './Pages/VerbuuApp';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/white_border" element={<BorderApp />} />
-      <Route path="/dermi" element={<BorderApp />} />
+      <Route path="/dermi" element={<DermiApp />} />
+      <Route path="/verbuu" element={<VerbuuApp />} />
       <Route path="*" element={<Home />} />
     </Routes>
   );
---
+++ src/Pages/DermiApp.tsx
@@ -0,0 +1,68 @@
+import '../App.css';
+
+function DermiApp() {
+  return (
+    <div className='box'>
+        <h2 className="linear-wipe">Dermi</h2>
+        <p>Privacy Policy</p>
+        <br></br>
+        <p>Last Updated: September 1, 2023</p>
+        <hr></hr>
+        <p>Welcome to our privacy policy.<br></br>We value your trust and are committed to protecting your privacy. This Privacy Policy is designed to help you understand how we collect, use, disclose, and safeguard your personal information when you use Dermi. By using Dermi, you agree to the practices described in this Privacy Policy.</p><br></br>
+        <div style={{textAlign: 'center'}}>
+            <p>
+                <br></br>
+                <h2>1. INFORMATION WE COLLECT</h2>
+
+                a. **Basic User Information**: To provide you with our services, we may collect and store the following basic user information:
+                  - Name
+                  - Email address
+
+                b. **User-Generated Content**: Dermi allows users to input photos and data related to their skincare routines. This data will be stored on our platform.
+
+                c. **Geo-location Data**: We may collect and store basic geo-location data for analytical purposes only. This data helps us improve our services and understand user behavior. 
+                    
+                    Google Analytics collects statistical data about our users' behavior in our Application, but it does not identify individual users to us.
+                    For a better understanding of what data Google Analytics collects and how it is used, we recommend that you review Google's privacy policy at https://policies.google.com/privacy.
+
+                <h2>2. HOW WE USE THE INFORMATION WE COLLECT</h2>
+
+                We use the information collected for the following purposes:
+
+                  a. To provide and improve our services.
+
+                  b. To communicate with you regarding updates, new features, and promotions.
+
+                  c. To analyze user behavior and preferences, and to enhance the user experience.
+
+                <h2>3. DATA STORAGE</h2>
+
+                All user data, including basic user information and user-generated content, is securely stored using Google Firebase. We do not sell your data to third parties.
+
+                <h2>4. CHILDREN'S PRIVACY</h2>
+
+                Dermi is not intended for use by individuals under the age of 13. We do not knowingly collect personal information from children under 13. If you believe we have collected personal information from a child under 13, please contact us immediately, and we will take steps to remove the information from our records.
+
+                <h2>5. SECURITY</h2>
+
+                We take the security of your personal information seriously and implement reasonable measures to protect it. However, no method of transmission over the internet or electronic storage is entirely secure, so we cannot guarantee absolute security.
+
+                <h2>6. CHANGES TO THIS PRIVACY POLICY</h2>
+
+                We may update this Privacy Policy from time to time to reflect changes in our practices or for other operational, legal, or regulatory reasons. We will notify you of any material changes by posting the updated Privacy Policy on Dermi.
+
+                <h2>7. CONTACT US</h2>
+
+                If you have any questions or suggestions about our Privacy Policy, do not hesitate to contact us.
+
+                Thank you for using Dermi. Your privacy is important to us.
+                
+                <br></br><br></br>
+                <a href = "mailto: sky@yea.dev">sky@yea.dev</a>
+            </p>
+        </div>
+    </div>
+  );
+}
+
+export default DermiApp;
