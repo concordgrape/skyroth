f7a3b776359ec93d172f21ef3a90dbee145bffa1
Sky Roth <sky.j.roth@gmail.com>
2024-10-29 10:10:35 -0400

    update dermi pages

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,18 @@ import { Route, Routes } from 'react-router-dom';
 
 import Home from './Pages/Home';
 import BorderApp from './Pages/BorderApp';
-import DermiApp from './Pages/DermiApp';
 import VerbuuApp from './Pages/VerbuuApp';
+
+import DermiPrvacy from './Pages/dermi/DermiPrivacy';
+import DermiEULA from './Pages/dermi/DermiEULA';
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/white_border" element={<BorderApp />} />
-      <Route path="/dermi" element={<DermiApp />} />
+      <Route path="/dermi/terms-of-use" element={<DermiEULA />} />
+      <Route path="/dermi/privacy-policy" element={<DermiPrvacy />} />
       <Route path="/verbuu" element={<VerbuuApp />} />
       <Route path="*" element={<Home />} />
     </Routes>
