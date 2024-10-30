5f355941c3c231fe7b333c60437ebb433c2d7b8f
Sky Roth <sky.j.roth@gmail.com>
2024-10-29 23:07:19 -0400

    Update URLs.js

--- src/Constants/URLs.js
+++ src/Constants/URLs.js
@@ -1,6 +1,6 @@
 export const GITHUB_LINK = "https://www.github.com/concordgrape";
 export const WHITEBORDER_LINK = "https://apps.apple.com/ca/app/white-border/id1574787497";
-export const DERMI_LINK = "https://apps.apple.com/us/app/dermi-skin-care-journal/id1570511480";
+export const DERMI_LINK = "https://dermi.app";
 export const LOREMGEN_LINK = "https://www.loremgen.com";
 export const HTMLCOLORS_LINK = "https://www.htmlcolors.co";
 export const CSSCOLORS_LINK = "https://www.csscolors.net";
