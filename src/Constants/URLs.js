7e3d1114aa7793f7cb701bae069e132bed1e5a0b
Sky Roth <sky.j.roth@gmail.com>
2025-01-30 17:17:04 -0500

    update

--- src/Constants/URLs.js
+++ src/Constants/URLs.js
@@ -2,6 +2,4 @@ export const GITHUB_LINK = "https://www.github.com/concordgrape";
 export const WHITEBORDER_LINK = "https://apps.apple.com/ca/app/white-border/id1574787497";
 export const DERMI_LINK = "https://dermi.app";
 export const LOREMGEN_LINK = "https://www.loremgen.com";
-export const HTMLCOLORS_LINK = "https://www.htmlcolors.co";
-export const CSSCOLORS_LINK = "https://www.csscolors.net";
 export const VERBUU_LINK = "https://www.verbuu.com";
--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -7,8 +7,6 @@ import {
 //  LINKS
 import { 
   GITHUB_LINK,
-  HTMLCOLORS_LINK,
-  CSSCOLORS_LINK,
   LOREMGEN_LINK,
   WHITEBORDER_LINK,
   DERMI_LINK,
@@ -23,23 +21,10 @@ function Home() {
         </AvatarWrapper>
         <LinksWrapper>
           <Header size={700}><a href={GITHUB_LINK} target="_blank" rel="noreferrer">github.com/concordgrape</a></Header>
-          {/*<br />
-          <OrLine>
-              <OrText>web</OrText>
-            </OrLine>
-          <Header size={700}><a href={VERBUU_LINK} target="_blank" rel="noreferrer">verbuu.com</a></Header>
-          <Header size={700}><a href={HTMLCOLORS_LINK} target="_blank" rel="noreferrer">htmlcolors.co</a> / <a href={CSSCOLORS_LINK} target="_blank" rel="noreferrer">csscolors.net</a></Header>
-          <Header size={700}><a href={LOREMGEN_LINK} target="_blank" rel="noreferrer">loremgen.com</a></Header>
-          <OrLine>
-              <OrText>app</OrText>
-            </OrLine>
-          <Header size={700}><a href={WHITEBORDER_LINK} target="_blank" rel="noreferrer">white_border</a></Header>
-          <Header size={700}><a href={DERMI_LINK} target="_blank" rel="noreferrer">dermi</a></Header>
-          */}
           <br />
           <OrLine>
-              <OrText>projects</OrText>
-            </OrLine>
+            <OrText>projects</OrText>
+          </OrLine>
           <Header size={700}><a href={VERBUU_LINK} target="_blank" rel="noreferrer">verbuu.com</a></Header>
           <Header size={700}><a href={LOREMGEN_LINK} target="_blank" rel="noreferrer">loremgen.com</a></Header>
           <Header size={700}><a href={WHITEBORDER_LINK} target="_blank" rel="noreferrer">white_border</a></Header>
@@ -52,15 +37,11 @@ function Home() {
 export default Home;
 
 const HomeWrapper = styled.div`
-  margin: 25em;
+  margin: 2em;
   margin-top: 4em;
   margin-bottom: 4em;
   justify-content: center;
   align-items: center;
-
-  @media only screen and (max-width: 768px) {
-    margin: 2em;
-  }
 `
 
 const AvatarWrapper = styled.div`
@@ -90,14 +71,11 @@ const Header = styled(Heading)`
 `
 const OrLine = styled.div`
   width: 100%; 
+  max-width: 500px;
   text-align: center; 
   border-bottom: 1px solid #B7B8B6; 
   line-height: 0.1em;
   margin: 30px 0 30px;
-
-  @media only screen and (max-width: 768px) {
-    margin: 50px 0 50px;
-  }
 `
 
 const OrText = styled.span`
