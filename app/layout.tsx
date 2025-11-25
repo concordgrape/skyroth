caa311681e2e8b1099b903768c58ecb3b77602da
Sky Roth <55454027+concordgrape@users.noreply.github.com>
2025-11-24 22:17:54 -0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next";
 
 export const metadata: Metadata = {
   title: "Sky Roth - Portfolio",
@@ -16,6 +17,7 @@ export default function RootLayout({
       <body
         className={`antialiased`}
       >
+        <Analytics />
         {children}
       </body>
     </html>
